6f0376e1109b9b7bdca4106ffa5a5fc3dbef8998
azizam68 <abdoul-aziz.mbodj@oclock.school>
2026-08-16 09:44:59 +0000

    fix pour le CI Workflow

--- src/lib/services/DocumentService.test.ts
+++ src/lib/services/DocumentService.test.ts
@@ -1,20 +1,25 @@
 import { describe, it, expect, vi } from 'vitest';
 import * as Y from 'yjs';
 import { DocumentService } from './DocumentService';
+import type { DocumentRepository } from '$lib/repositories/DocumentRepository';
+
+const mockRepository: DocumentRepository = {
+  save: vi.fn(),
+  load: vi.fn().mockResolvedValue(null),
+  //delete: vi.fn().mockResolvedValue(undefined),
+  //list: vi.fn().mockResolvedValue([]),
+};
 
 describe('DocumentService', () => {
     it('sauvegarde un document via le repository', async () => {
-        const repository = {
-            save: vi.fn()
-        };
 
-        const service = new DocumentService(repository);
+        const service = new DocumentService(mockRepository);
 
         const ydoc = new Y.Doc();
 
         await service.save('document-1', ydoc);
 
-        expect(repository.save).toHaveBeenCalledWith(
+        expect(mockRepository.save).toHaveBeenCalledWith(
             'document-1',
             ydoc
         );
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
-	import Editor from '$lib/Editor.svelte';
+  import Editor from "$lib/Editor.svelte";
+  import * as Y from "yjs";
+  
+  const ydoc = new Y.Doc();
+  
+  ydoc.on('update', () => {
+    console.log('Document updated');
+  });
+
 </script>
 
 <h1>Welcome to Jixie</h1>
 <p>dev in progress ...</p>
 
-<Editor />
+<Editor {ydoc} />
